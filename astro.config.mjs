6d3d372ede9e5084c022b2f21fd5048d46d86c16
taketaketaketake <zach@takedetroit.com>
2025-10-16 15:21:47 -0400

    adapter fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // astro.config.mjs
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()],
   vite: {
     define: {
